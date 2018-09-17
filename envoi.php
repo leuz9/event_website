f42d8f667bca783403e4bd06d969244991bd9cbc
Roland Niokhor FAYE <35190101+leuz9@users.noreply.github.com>
2018-09-17 09:24:30 +0000

    Add files via upload

--- envoi.php
+++ envoi.php
@@ -1,15 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Donn&eacute;es envoy&eacute;es</title>
+</head>
+
+<body>
 <?php 
-// using SendGrid's PHP Library
-// https://github.com/sendgrid/sendgrid-php
-require("sendgrid/sendgrid-php.php");
-$sendgrid = new SendGrid("SENDGRID_APIKEY");
-$email    = new SendGrid\Email();
+//Pour définir chaque input du formulaire, ajouter le signe de dollar devant
 
-$email->addTo("leuzleuz9@gmail.com")
-      ->setFrom("jobrek17@gmail.com")
-      ->setSubject("Sending with SendGrid is Fun")
-      ->setHtml("and easy to do anywhere, even with PHP");
+$nom = $_POST['nom'];
+$mailheaders .= $_POST['email'];
+$msg .= $_POST['message'];
+//Pourait continuer ainsi jusqu'à la fin du formulaire
 
-$sendgrid->send($email);
+$recipient = "leuzleuz9@gmail.com";
+$subject = "Formulaire";
+
+
+mail($recipient, $subject, $msg, $mailheaders);
+
+
+echo "<H1 align=center>Merci, $nom </H1>";
+
+
+
+
 
 ?>
+
+
+Les donn&eacute;es ont &eacute;t&eacute; envoy&eacute;s, cliquez <a href="#">ici pour retourner sur le site 
+</a>
+</body>
+</html>
